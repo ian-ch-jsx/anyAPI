17b1440f1e1446556991c1d636359e8726216a19
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-15 15:45:33 -0700

    findById working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Monster = require('../lib/models/Monster');
 
 describe('anyAPI routes', () => {
   beforeEach(() => {
@@ -30,5 +31,12 @@ describe('anyAPI routes', () => {
     const res = await request(app).post('/api/v1/monsters').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a monster by id', async () => {
+    const expected = await Monster.findById(1);
+    const res = await request(app).get(`/api/v1/monsters/${expected.id}`);
+
+    expect(res.body).toEqual({ ...expected });
   });
 });
